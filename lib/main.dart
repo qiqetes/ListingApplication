e531c046cd3fadf89ba236cb3aaa0fe5b24bdba0
Enrique Llopis <enrique@bestcycling.es>
2023-04-22 12:51:53 +0200

    ignore non target builds

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:interview_gpt/bloc/chat/chat_bloc.dart';
+import 'package:interview_gpt/pages/display_listings/listings_page.dart';
 import 'package:interview_gpt/pages/home/home_page.dart';
-import 'package:interview_gpt/pages/listings_page.dart';
 
 void main() {
   runApp(
